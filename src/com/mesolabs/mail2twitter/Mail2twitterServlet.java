73fb388e71496dbfb06b172f7bc0387d1a271c9a
Toshihiro Shimizu <shimizu.toshihiro@gmail.com>
2010-06-15 14:37:30 +0900

    初期コミット

---
+++ src/com/mesolabs/mail2twitter/Mail2twitterServlet.java
@@ -0,0 +1,13 @@
+package com.mesolabs.mail2twitter;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Mail2twitterServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
